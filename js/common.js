1596e6243e6970cd51a8d21ddd1aa9018648b6f3
User121118 <44980874+User121118@users.noreply.github.com>
2019-01-04 14:11:26 +0300

    Well-Done 1.0
    Отправка работы на защиту.

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
+"use strict";
 var link = document.querySelector(".booking-btn");
 var popup = document.querySelector("form.booking-form");
- link.addEventListener("click", function (evt) {
-evt.preventDefault();
-      popup.classList.toggle("booking-show");
-  });
+popup.classList.remove("booking-form-on");
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("booking-show");
+});
